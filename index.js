8594e7858d1d78315d9ee8f17c75259ab44dae5f
Darrach Barneveld <darrachbarneveld@Darrachs-MacBook-Pro.local>
2023-01-14 17:37:52 +0000

    added notificaiton

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const DUMMY_DATA = require("./testData.json");
 const randomFromArray = require("./randomFromArray");
+const notifications = require("./notifications");
 
 const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/api", async (req, res) => {
   const randomQuote = randomFromArray(DUMMY_DATA);
+
+  const response = await notifications.sendPushNotification(
+    "Token ExponentPushToken[lWVYdEKN8YfYnpnKFW0koX]",
+    "'This is a test push notification'"
+  );
+
+  console.log(response);
 
   res.json(randomQuote);
 });
---
+++ notifications.js
@@ -0,0 +1,21 @@
+const Expo = require("expo-server-sdk");
+
+exports.sendPushNotification = async (pushToken, message) => {
+  try {
+    // Create a new Expo object
+    const expo = new Expo();
+    // create the notification data object
+    const notification = {
+      to: pushToken,
+      sound: "default",
+      body: message,
+    };
+    // Add the notification to the Expo object
+    expo.sendPushNotification(notification);
+    // Send the notification
+    const response = await expo.sendPushNotificationsAsync();
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
