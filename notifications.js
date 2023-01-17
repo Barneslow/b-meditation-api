69ae13f1889689f280f4f2d7c1d75f6b9e05d3c4
Darrach Barneveld <darrachbarneveld@Darrachs-MacBook-Pro.local>
2023-01-17 14:32:40 +0000

    updated expo notification function

--- notifications.js
+++ notifications.js
@@ -1,20 +1,40 @@
 const { Expo } = require("expo-server-sdk");
 
-exports.sendPushNotification = async (pushToken, message) => {
+exports.sendPushNotification = async (pushToken, title, message) => {
   try {
     // Create a new Expo object
     const expo = new Expo();
     // create the notification data object
-    const notification = {
+
+    let messages = [];
+
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      return;
+    }
+
+    messages.push({
       to: pushToken,
+      title,
       sound: "default",
       body: message,
-    };
-    // Add the notification to the Expo object
-    expo.sendPushNotification(notification);
-    // Send the notification
-    const response = await expo.sendPushNotificationsAsync();
-    console.log(response);
+      data: { withSome: "data" },
+    });
+
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+
+    (async () => {
+      for (let chunk of chunks) {
+        try {
+          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+
+          tickets.push(...ticketChunk);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
   } catch (error) {
     console.log(error);
   }
